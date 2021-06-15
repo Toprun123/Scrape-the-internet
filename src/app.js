82e8303bf24a15d057eb6f3a1402eff5d51a6319
Toprun123 <syeddaanish2000@gmail.com>
2021-06-15 09:36:39 +0300

    minor changes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 //element_by_class.apply({out: 'id'}, ['mln4 mb24 sm:d-none svg-spot spotCookieLg', 'https://stackoverflow.com', 'svg',"html"]);
 contents_as_out.apply({out: 'id'}, ['https://pages.github.com/', 'div', 'slideshow', 'html']);
+
 // BUG: Returns "Undefined"
 //let then = Sti("https://stackoverflow.com", "div", "left-sidebar", "html");
 //document.getElementById('h3').innerHTML = then;
